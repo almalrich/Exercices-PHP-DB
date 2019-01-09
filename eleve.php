98a3e01e35ba652cba4a2a974097cfd2e435599f
almalrich <seb-bastos16@live.fr>
2019-01-09 14:45:01 +0100

    2er commit -password

---
+++ eleve.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrateur
+ * Date: 09/01/2019
+ * Time: 11:27
+ */
+
+$servername = "localhost";
+$username = "id7331190_seb";
+$password = "bastos16";
+$dbname = "id7331190_nouvelles";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error){
+
+    die("Connection failed: " . $conn->connect_error);
+}
+else
+{
+// Selectionner la base à utiliser
+    $conn->select_db($dbname);
+
+}
+
+$id = $_GET['id'];
+
+function mugsEleve ($id_eleve, $conn)
+{
+
+    $sql = "SELECT mugs.description FROM eleve_mugs,mugs WHERE eleve_mugs.id_eleve = '$id_eleve' and eleve_mugs.id_mugs = mugs.id ";
+    $resultat = $conn->query($sql);
+    $mugs = [];
+    while($row = $resultat->fetch_assoc()){
+        array_push($mugs, $row);
+    }
+    return $mugs;
+}
+$test = mugsEleve($_GET['id'], $conn);
+
+for ($i=0; $i<count('SELECT id FROM mugs');$i++){
+    echo $test[$i]['description'];
+
+
+
+}
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -44,25 +44,28 @@ global $conn;
     $sql = "SELECT * FROM eleves";
 $result = $conn->query($sql);
 while($row = $result->fetch_assoc())
-{ echo $row['prenom']." ". $row['nom']." ".$row['age']."<br>";}
+{ echo $row['id']." ".$row['prenom']." ". $row['nom']." ".$row['age']." ";
+echo "<a href='eleve.php?id=".$row ['id']."'>voir l'eleve</a><br>";
+}
 }
 
 //name();
 
-function deletEleve ($id, $conn){
+function deletEleves ($id, $conn){
 
-    $sql = "DELETE from eleves where id = $id"; $conn->query($sql);
-
-
+    $sql = "DELETE from eleves where id = $id";
+    $conn->query($sql);
 
 }
-//deletEleve(10,$conn);
+//deletEleves(10,$conn);
 
 function mettreAJour($id, $prenom, $nom, $age, $conn){
     $sql= "UPDATE eleves set prenom='$prenom', nom='$nom', age='$age' WHERE id='$id'";
     $conn->query($sql);
 }
 //mettreAJour(11, "seb", "bon", 26, $conn );
+
+
 
 if (isset($_GET['nom'])&& isset($_GET['prenom']) && isset($_GET['age']) )
 {
@@ -72,9 +75,75 @@ if (isset($_GET['nom'])&& isset($_GET['prenom']) && isset($_GET['age']) )
 
     ana($nouveauPrenom,$nouveauNom,$nouveauAge,$conn);
 
+
+}
+
+if (isset($_GET['id']) )
+{
+    $index= $_GET['id'];
+
+    $sql = "DELETE from eleves where id = '$index'";
+    $conn->query($sql);
+
+
+    deletEleves($index,$conn);
+
+}
+
+if (!empty($_GET['afficher'])){
+    name();
+}
+
+if (isset($_GET['mettre_a_jour'])){
+   mettreAJour($_GET['id1'],$_GET['prenom1'], $_GET['nom1'], $_GET['age1'], $conn );
 }
 
 
+function mugs ($description, $conn)
+{
+    $sql = "INSERT INTO mugs VALUES (NULL,'$description')";
+
+    $conn->query($sql);
+}
+
+if (isset ($_GET['couleur'])) {
+
+    $nouvelleCouleur = $_GET["couleur"];
+
+    mugs($nouvelleCouleur, $conn);
+}
+
+function associatier($id_eleve, $id_mugs){
+
+
+    global $conn;
+
+    $sql = "INSERT INTO eleve_mugs VALUES (NULL,'$id_eleve', '$id_mugs')";
+    $conn->query($sql);
+
+}
+
+if (isset($_GET['nom'])&& isset($_GET['mugs'])) {
+    associatier($_GET['nom2'], $_GET['mugs']);
+}
+function mugsEleve ($id_eleve, $conn)
+{
+    $sql = "SELECT * FROM eleves WHERE id='$id_eleve'";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc())
+    { echo $row['prenom']." ". $row['nom']." ";
+
+    }
+  $sql = "SELECT mugs.description FROM eleve_mugs,mugs WHERE eleve_mugs.id_eleve = '$id_eleve' and eleve_mugs.id_mugs = mugs.id ";
+    $conn->query($sql);
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc())
+    { echo $row['description']." ";
+
+    }
+}
+
+mugsEleve($_GET['id6'],$conn);
 
 ?>
 
@@ -92,10 +161,46 @@ if (isset($_GET['nom'])&& isset($_GET['prenom']) && isset($_GET['age']) )
    prenom: <input type="text" name="prenom"><br>
     nom: <input type="text" name="nom"><br>
     age: <input type="text" name="age"><br>
+    <input type="submit" value="ajouter"><br>
+
+</form>
+
+
+<form action="index.php" method="get">
+id: <input type="text" name="id"><br>
+<input type="submit" name="enlever" value="enlever"><br>
+</form>
+
+
+<form action="index.php" method="get">
+    <input type="submit" name="afficher" value="afficher"><br>
+</form>
+
+<form action="index.php" method="get">
+    id:<input type="text" name="id1"><br>
+    prenom: <input type="text" name="prenom1"><br>
+    nom: <input type="text" name="nom1"><br>
+    age: <input type="text" name="age1"><br>
+    <input type="submit" name="mettre_a_jour" value="mettre_a_jour"><br>
+
+</form>
+
+<form action="index.php" method="get">
+  couleur du mugs: <input type="text" name="couleur"><br>
     <input type="submit" value="ajouter">
 
 </form>
 
+<form action="index.php" method="get">
+    eleve: <input type="text" name="nom2"><br>
+    mugs: <input type="text" name="mugs"><br>
+    <input type="submit" value="mugs">
+
+</form>
+
+<form action="index.php" method="get">
+id: <input type="text" name="id6"><br>
+<input type="submit" name="id7" value="mugs">
 
 </body>
 </html>
